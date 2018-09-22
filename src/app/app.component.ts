4babe798ff5f1dba6527331fa540cd110f20bbc6
Pedro <pedrozo_96@hotmail.com>
2018-09-22 12:30:42 -0300

    Adding create reservation, view reservations, delete reservations, delete places, and etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,125 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
+import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { User } from './user';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/switchMap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root',  //tag de html que va a reconocer angular para poder insertar este componente
+  templateUrl: './app.component.html', //archivo html que va a usar este componente
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  form: FormGroup;
+  user: BehaviorSubject<User> = new BehaviorSubject(null)
+  authenticated: boolean = false;
+  loggedUser:any= null;
+  profilePhoto:any = null;
+
+  constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase, private router:Router, private fb: FormBuilder) {
+    this.createForm();
+
+    this.afAuth.authState
+        .switchMap(auth => {
+          if (auth) {
+            /// signed in
+            this.authenticated = true;
+            return this.db.object('users/' + auth.uid)
+
+          } else {
+            /// not signed in
+            return Observable.of(null)
+          }
+        })
+        .subscribe(user => {
+          this.user.next(user)
+          this.loggedUser = this.user.value.email;
+          this.profilePhoto = this.user.value.photoURL;
+          console.log(this.loggedUser);
+          console.log(this.profilePhoto);
+
+        })
+    }
+
+
+
+    createForm() {
+      this.form = this.fb.group({
+        name: ['', Validators.required],
+        email: ['', Validators.required],
+        message: ['', Validators.required],
+      });
+    }
+    onSubmit() {
+      const {name, email, message} = this.form.value;
+      const date = Date();
+      const html = `
+        <div>From: ${name}</div>
+        <div>Email: <a href="mailto:${email}">${email}</a></div>
+        <div>Date: ${date}</div>
+        <div>Message: ${message}</div>
+      `;
+      let formRequest = { name, email, message, date, html };
+      this.db.list('/messages').push(formRequest);
+      this.form.reset();
+    }
+
+    ngOnInit() {
+    }
+    ///// SignIn - SignOut Process /////
+    googleLogin() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      return this.afAuth.auth.signInWithPopup(provider)
+        .then(credential =>  {
+            this.updateUser(credential.user)
+        })
+        .then((result)=>{
+          this.authenticated = true;
+          console.log('Signed in successfully!');
+        }).catch((error)=>{
+          this.authenticated = false;
+          console.log('Error signing in: ',error);
+        });
+    }
+
+
+
+    signOut() {
+      this.afAuth.auth.signOut()
+      .then((result)=>{
+        this.router.navigate(['']).then(function(){
+          window.location.reload();
+          this.authenticated = false;
+        });
+        console.log('You were logged out successfully!');
+      }).catch((error) =>{
+        this.authenticated = true;
+        console.log('Error signing out: ',error);
+      })
+    }
+    //// Update user data ////
+    /// updates database with user info after login
+    /// only runs if user role is not already defined in database
+    private updateUser(authData) {
+      const userData = new User(authData)
+      const ref = this.db.object('users/' + authData.uid)
+      ref.take(1)
+         .subscribe(user => {
+          if (!user.role) {
+            ref.update(userData)
+          }
+      })
+    }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,156 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from "./directives/resaltar.directive";
+import { ContarClicksDirective } from "./directives/contador-clicks.directive";
+import { Routes, RouterModule } from "@angular/router";
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { CrearComponent } from "./crear/crear.component";
+import { LinkifystrPipe } from "./pipes/linkifystr.pipe";
+import { DynamicContent }  from "./directives/dynamic-content.directive";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatProgressSpinnerModule,
+  MatTabsModule,
+  MatListModule,
+  MatCheckboxModule, MatGridListModule, MatInputModule, MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+
+//Http
+import { HttpModule } from "@angular/http";
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+
+//share buttons
+import { ShareButtonsModule } from '@ngx-share/buttons';
+
+//Forms
+import { FormsModule, ReactiveFormsModule} from "@angular/forms"
 
 
-@NgModule({
-  declarations: [
-    AppComponent
+//Components
+import { NavbarComponent } from './navbar/navbar.component';
+import { ReservacionesComponent } from './reservaciones/reservaciones.component';
+import { DetalleComponent } from "./detalle/detalle.component";
+import { LugaresComponent } from "./lugares/lugares.component";
+import { ContactoComponent } from "./contacto/contacto.component";
+// import { FormUploadComponent } from './form-upload/form-upload.component';
+// import { UploadsListComponent } from './uploads-list/uploads-list.component';
+// import { UploadDetailComponent } from './upload-detail/upload-detail.component';
+import { EditReservacionComponent } from './edit-reservacion/edit-reservacion.component';
+import { DeleteReservacionComponent } from './delete-reservacion/delete-reservacion.component';
+import { DeleteLugarComponent } from './delete-lugar/delete-lugar.component';
+
+//Modules
+
+
+//Services
+import {LugaresService} from "./services/lugares.service";
+import { GeoService } from "./services/geo.service";
+// import { UploadFileService } from "./services/uploadfile.service";
+import { AuthorizationService } from './services/authorization.service'
+import { ReservacionesService } from './services/reservaciones.service'
+
+
+
+
+//firebase configuration
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+
+
+
+
+
+
+
+const appRoutes : Routes = [
+  {path: '', component: LugaresComponent},
+  {path: 'lugares', component: LugaresComponent},
+  {path: 'detalle/:id', component: DetalleComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'crear/:id', component: CrearComponent},
+  {path: 'reservaciones', component: ReservacionesComponent},
+  {path: 'edit-reservacion/:id', component: EditReservacionComponent},
+  {path:'delete-reservacion/:id', component: DeleteReservacionComponent},
+  {path:'delete-lugar/:id', component: DeleteLugarComponent}
+
+
+
+
+
+];
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBQ1F6d0gP_SCMuHBQVt3mGTReuL3baRMQ",
+    authDomain: "venztecmaps-1516388139367.firebaseapp.com",
+    databaseURL: "https://venztecmaps-1516388139367.firebaseio.com",
+    storageBucket: "venztecmaps-1516388139367.appspot.com",
+    messagingSenderId: "767470847086"
+  }
+};
+//cada componente debe estar atado a un modulo para que sean funcionales, ejemplo modulo RRHH, tiene tres componentes llamados pagos, empleados e incidencias
+@NgModule({//un componente de cada app de a4 es por ejemplo si hago una pag cada una de las vistas deberia ser un componente por buena practica
+  declarations: [  //componentes, nos representan una seccion de nuestra app en nuestro modulo, puede ser la pag de contactos, o nosotros, cada una va a ser un componente que ira aqui
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent,
+    ContactoComponent,
+    CrearComponent,
+    LinkifystrPipe,
+    DynamicContent,
+    // FormUploadComponent,
+    // UploadsListComponent,
+    // UploadDetailComponent,
+    NavbarComponent,
+    ReservacionesComponent,
+    EditReservacionComponent,
+    DeleteReservacionComponent,
+    DeleteLugarComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  //llamamos los modulos que vamos a ocupar para nuestra aplicacion
+    BrowserModule,
+    // HttpModule,
+     FormsModule,
+     AgmCoreModule.forRoot({
+  apiKey: 'AIzaSyB6T1XN1ryjOVtyf_fu48QuWWYRXdh0xXk'
+}),
+     RouterModule.forRoot(appRoutes),
+     AngularFireModule.initializeApp(environment.firebase),
+ // imports firebase/app needed for everything
+     // AngularFireModule.initializeApp(firebaseConfig),
+     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+     AngularFireDatabaseModule,
+     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+     HttpModule,
+     BrowserAnimationsModule,
+     MatButtonModule,
+     MatMenuModule,
+     MatCardModule,
+     MatToolbarModule,
+     MatIconModule,
+     MatCheckboxModule,
+     MatGridListModule,
+     MatInputModule,
+     MatDatepickerModule,
+     MatNativeDateModule,
+     MatProgressSpinnerModule,
+     MatTabsModule,
+     MatListModule,
+     AgmSnazzyInfoWindowModule,
+     ReactiveFormsModule,
+     HttpClientModule,       // for share counts
+     HttpClientJsonpModule,  // for linkedin and tumblr share counts
+     ShareButtonsModule.forRoot()
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LugaresService, GeoService, AuthorizationService, ReservacionesService], //aqui ponemos los servicios que vamos a necesitar para nuestra app
+  bootstrap: [AppComponent] //aqui solo colocamos con cual componente vamos a iniciar
 })
 export class AppModule { }
